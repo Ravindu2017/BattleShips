0c525c33a27fb456300c6d4e165b2ae096fca219
Ravindu2017 <ravindu.hasanka90@gmail.com>
2024-06-05 14:10:17 +0300

    added new files and changed code behaviour

--- gamePlan.js
+++ gamePlan.js
@@ -1,1 +1,67 @@
 // First Commit
+// const { Position } = require("./battleShips.html")
+class Position {
+    constructor(width, depth){
+        this.width = width;
+        this.depth = depth;
+        this.hit = null;
+        this.ship = null;
+    }
+}
+
+class Gameboard {
+    constructor(board) {
+        this.board = {};
+    }
+    
+    append(x, y){
+        if (this.board[x+y]) {
+            return "object exists"
+        }
+
+        const squares = new Position(x, y);
+        const key = squares.width + squares.depth;
+
+        this.board[key] = squares;
+
+        // this.gameState
+
+    }
+
+    createBoardGame() {
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                const location = new Position(String.fromCharCode(65+i), j+1);
+                // gameState.append(location.width, location.depth);
+                this.append(location.width, location.depth);
+            }
+        }
+    }
+}
+
+const gameState = new Gameboard();
+// gameState.append("A", 10);
+
+const what = new Position("A", 10);
+// gameState.append(what.width, what.depth);
+
+// function createBoardGame(obj) {
+//     for (let i = 0; i < 10; i++) {
+//         for (let j = 0; j < 10; j++) {
+//             const location = new Position(String.fromCharCode(65+i), j+1);
+//             // gameState.append(location.width, location.depth);
+//             obj.append(location.width, location.depth);
+//         }
+//     }
+// }
+
+// createBoardGame(gameState);
+
+gameState.createBoardGame();
+
+console.log(gameState.board["A10"].hit, gameState.board["F2"].ship, what, String.fromCharCode(65))//, charCodeAt(165))
+console.log(gameState.board);
+// module.exports = {
+//     Position,
+//     Gameboard
+// }
